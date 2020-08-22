6766e1458d6d528696fefb12be427d68b4d4beb0
xlordnoro <69708902+xlordnoro@users.noreply.github.com>
2020-08-22 01:41:59 -0400

    Create playcools_js_code.js

---
+++ playcools_js_code.js
@@ -0,0 +1,142 @@
+// <![CDATA[
+    jQuery(function($) {
+       function show_sixth_season_bd720() {
+            $('#sixth_season_bd1080pane').hide();
+            $('#sixth_season_bd1080on').fadeTo('fast', 0.4);
+            $('#sixth_season_bd720on').fadeTo('fast', 1.0);
+            $('#sixth_season_bd720pane').slideDown('slow', function() {
+            });
+        }
+        function show_sixth_season_bd1080() {
+            $('#sixth_season_bd720pane').hide();
+            $('#sixth_season_bd1080on').fadeTo('fast', 1);
+            $('#sixth_season_bd720on').fadeTo('fast', 0.4);
+            $('#sixth_season_bd1080pane').slideDown('slow', function() {
+            });
+        }
+        $('#sixth_season_bd720').click(function(event) {
+            event.preventDefault();
+            show_sixth_season_bd720();
+        });
+        $('#sixth_season_bd1080').click(function(event) {
+            event.preventDefault();
+            show_sixth_season_bd1080();
+        });
+        function show_fifth_season_bd720() {
+            $('#fifth_season_bd1080pane').hide();
+            $('#fifth_season_bd1080on').fadeTo('fast', 0.4);
+            $('#fifth_season_bd720on').fadeTo('fast', 1.0);
+            $('#fifth_season_bd720pane').slideDown('slow', function() {
+            });
+        }
+        function show_fifth_season_bd1080() {
+            $('#fifth_season_bd720pane').hide();
+            $('#fifth_season_bd1080on').fadeTo('fast', 1);
+            $('#fifth_season_bd720on').fadeTo('fast', 0.4);
+            $('#fifth_season_bd1080pane').slideDown('slow', function() {
+            });
+        }
+        $('#fifth_season_bd720').click(function(event) {
+            event.preventDefault();
+            show_fifth_season_bd720();
+        });
+        $('#fifth_season_bd1080').click(function(event) {
+            event.preventDefault();
+            show_fifth_season_bd1080();
+        });
+        function show_fourth_season_bd720() {
+            $('#fourth_season_bd1080pane').hide();
+            $('#fourth_season_bd1080on').fadeTo('fast', 0.4);
+            $('#fourth_season_bd720on').fadeTo('fast', 1.0);
+            $('#fourth_season_bd720pane').slideDown('slow', function() {
+            });
+        }
+        function show_fourth_season_bd1080() {
+            $('#fourth_season_bd720pane').hide();
+            $('#fourth_season_bd1080on').fadeTo('fast', 1);
+            $('#fourth_season_bd720on').fadeTo('fast', 0.4);
+            $('#fourth_season_bd1080pane').slideDown('slow', function() {
+            });
+        }
+        $('#fourth_season_bd720').click(function(event) {
+            event.preventDefault();
+            show_fourth_season_bd720();
+        });
+        $('#fourth_season_bd1080').click(function(event) {
+            event.preventDefault();
+            show_fourth_season_bd1080();
+        });
+        function show_third_season_bd720() {
+            $('#third_season_bd1080pane').hide();
+            $('#third_season_bd1080on').fadeTo('fast', 0.4);
+            $('#third_season_bd720on').fadeTo('fast', 1.0);
+            $('#third_season_bd720pane').slideDown('slow', function() {
+            });
+        }
+        function show_third_season_bd1080() {
+            $('#third_season_bd720pane').hide();
+            $('#third_season_bd1080on').fadeTo('fast', 1);
+            $('#third_season_bd720on').fadeTo('fast', 0.4);
+            $('#third_season_bd1080pane').slideDown('slow', function() {
+            });
+        }
+        $('#third_season_bd720').click(function(event) {
+            event.preventDefault();
+            show_third_season_bd720();
+        });
+        $('#third_season_bd1080').click(function(event) {
+            event.preventDefault();
+            show_third_season_bd1080();
+        });
+        function show_second_season_bd720() {
+            $('#second_season_bd1080pane').hide();
+            $('#second_season_bd1080on').fadeTo('fast', 0.4);
+            $('#second_season_bd720on').fadeTo('fast', 1.0);
+            $('#second_season_bd720pane').slideDown('slow', function() {
+            });
+        }
+        function show_second_season_bd1080() {
+            $('#second_season_bd720pane').hide();
+            $('#second_season_bd1080on').fadeTo('fast', 1);
+            $('#second_season_bd720on').fadeTo('fast', 0.4);
+            $('#second_season_bd1080pane').slideDown('slow', function() {
+            });
+        }
+        $('#second_season_bd720').click(function(event) {
+            event.preventDefault();
+            show_second_season_bd720();
+        });
+        $('#second_season_bd1080').click(function(event) {
+            event.preventDefault();
+            show_second_season_bd1080();
+        });
+        function show_first_season_bd720() {
+            $('#first_season_bd1080pane').hide();
+            $('#first_season_bd1080on').fadeTo('fast', 0.4);
+            $('#first_season_bd720on').fadeTo('fast', 1.0);
+            $('#first_season_bd720pane').slideDown('slow', function() {
+            });
+        }
+        function show_first_season_bd1080() {
+            $('#first_season_bd720pane').hide();
+            $('#first_season_bd1080on').fadeTo('fast', 1);
+            $('#first_season_bd720on').fadeTo('fast', 0.4);
+            $('#first_season_bd1080pane').slideDown('slow', function() {
+            });
+        }
+        $('#first_season_bd720').click(function(event) {
+            event.preventDefault();
+            show_first_season_bd720();
+        });
+        $('#first_season_bd1080').click(function(event) {
+            event.preventDefault();
+            show_first_season_bd1080();
+        });
+        show_first_season_bd1080();
+        show_second_season_bd1080();
+        show_third_season_bd1080();
+        show_fourth_season_bd1080();
+        show_fifth_season_bd1080();
+        show_sixth_season_bd1080();
+    });
+// ]]>

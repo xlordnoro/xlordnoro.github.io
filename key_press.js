9022bedf2c5ee448107525fe49952580d8dd5655
xlordnoro <69708902+xlordnoro@users.noreply.github.com>
2020-12-12 22:34:15 -0500

    Update key_press.js

--- key_press.js
+++ key_press.js
@@ -1,23 +1,26 @@
 jQuery(function($) {
-var $keys = [];
+        var $keys = [];
         var $code = '84,65,77,65';
         var $original_src = $('#cover').attr('src');
         var $hidden_src = 'https://imgur.com/JYVtk2Z.jpg';
-        (new Image()).src = $hidden_src;
+        (new Image()).src = $hidden_src; // preload the hidden image
         $(document)
         .keydown(function(event) {
             $keys.push( event.keyCode );
             if ( $keys.toString().indexOf( $code ) >= 0 ) {
+                // do something when the konami code is executed
                 $('#cover').stop().fadeOut(function() {
                     var $current_src = $('#cover').attr('src');
                     var $new_src = $current_src==$original_src ? $hidden_src : $original_src;
                     $('#cover').attr('src', $new_src).fadeIn();
                 });
+                // empty the array containing the key sequence entered by the user
                 $keys = [];
+                // cache limiter
                 if ( $keys.toString().length >= $code.length ) {
                     $keys.shift();
                 }
             }
         }
-        );
+    );
 });

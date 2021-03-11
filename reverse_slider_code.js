7f248b36e9959d404b3782654d8793bc4ac89398
xlordnoro <69708902+xlordnoro@users.noreply.github.com>
2021-03-11 00:13:09 -0500

    Create reverse_slider_code.js
    A reverse variant of the slider code.

---
+++ reverse_slider_code.js
@@ -0,0 +1,23 @@
+jQuery(function($) {
+        function show_bd1080() {
+            $('#bd720pane').hide();
+            $('#bd1080on').fadeTo('fast', 1);
+            $('#bd720on').fadeTo('fast', 0.4);
+            $('#bd1080pane').slideDown('slow');
+        }
+        function show_bd720() {
+            $('#bd1080pane').hide();
+            $('#bd1080on').fadeTo('fast', 0.4);
+            $('#bd720on').fadeTo('fast', 1.0);
+            $('#bd720pane').slideDown('slow');
+        }
+        $('#bd1080on').click(function(event) {
+            event.preventDefault();
+            show_bd1080();
+        });
+        $('#bd720on').click(function(event) {
+            event.preventDefault();
+            show_bd720();
+        });
+        show_bd720();
+    });

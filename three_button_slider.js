6f27edef6510c652af660c416efe233ab62f3159
xlordnoro <69708902+xlordnoro@users.noreply.github.com>
2021-05-13 00:10:15 -0400

    Create three_button_slider.js
    Expands the original slider code and uses three buttons instead of the default of two.

---
+++ three_button_slider.js
@@ -0,0 +1,42 @@
+jQuery(function($) {
+        function show_bd1080() {
+            $('#bd720pane').hide();
+            $('#moviepane').hide();
+            $('#bd1080on').fadeTo('fast', 1);
+            $('#bd720on').fadeTo('fast', 0.4);
+            $('#movieon').fadeTo('fast', 0.4);
+            $('#bd1080pane').slideDown('slow', function() {
+            });
+        }
+        function show_bd720() {
+            $('#bd1080pane').hide();
+            $('#moviepane').hide();
+            $('#bd1080on').fadeTo('fast', 0.4);
+            $('#movieon').fadeTo('fast', 0.4);
+            $('#bd720on').fadeTo('fast', 1.0);
+            $('#bd720pane').slideDown('slow', function() {
+            });
+        }
+        function show_movie() {
+            $('#bd1080pane').hide();
+            $('#bd720pane').hide();
+            $('#bd1080on').fadeTo('fast', 0.4);
+            $('#bd720on').fadeTo('fast', 0.4);
+            $('#movieon').fadeTo('fast', 1.0);
+            $('#moviepane').slideDown('slow', function() {
+            });
+        }
+        $('#bd1080').click(function(event) {
+            event.preventDefault();
+            show_bd1080();
+        });
+        $('#bd720').click(function(event) {
+            event.preventDefault();
+            show_bd720();
+        });
+        $('#movie').click(function(event) {
+            event.preventDefault();
+            show_movie();
+        });
+        show_bd720();
+    });

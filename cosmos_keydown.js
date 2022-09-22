62bab53c01241fb0a35cf7578fb3bf2c12b3fb61
xlordnoro <69708902+xlordnoro@users.noreply.github.com>
2022-09-22 01:07:08 -0400

    Create cosmos_keydown.js
    Changes oresuki post cover image to our loving samurai and student council president, Cosmos.

---
+++ cosmos_keydown.js
@@ -0,0 +1,26 @@
+jQuery(function($) {
+        var $keys = [];
+        var $code = '67,79,83,77,79,83';
+        var $original_src = $('#cover').attr('src');
+        var $hidden_src = 'https://imgur.com/hsMfLGM.jpg';
+        (new Image()).src = $hidden_src; // preload the hidden image
+        $(document)
+        .keydown(function(event) {
+            $keys.push( event.keyCode );
+            if ( $keys.toString().indexOf( $code ) >= 0 ) {
+                // do something when the konami code is executed
+                $('#cover').stop().fadeOut(function() {
+                    var $current_src = $('#cover').attr('src');
+                    var $new_src = $current_src==$original_src ? $hidden_src : $original_src;
+                    $('#cover').attr('src', $new_src).fadeIn();
+                });
+                // empty the array containing the key sequence entered by the user
+                $keys = [];
+                // cache limiter
+                if ( $keys.toString().length >= $code.length ) {
+                    $keys.shift();
+                }
+            }
+        }
+    );
+});

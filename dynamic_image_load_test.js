d2b33157d8252b3c963f8ef62a8ca46206bb6f7c
xlordnoro <69708902+xlordnoro@users.noreply.github.com>
2023-12-01 14:53:19 -0500

    Create dynamic_image_load_test.js
    Testing dynamic loading of images based on their img title to streamline image corrections from imgur deletions.

---
+++ dynamic_image_load_test.js
@@ -0,0 +1,37 @@
+jQuery(function($) {
+        // Function to update the image source based on the img title
+        function updateImage() {
+            // Get the current img title from the img tag
+            var imgTitle = $("#tabImage").attr("title");
+
+            // If the title is empty, do nothing
+            if (!imgTitle) {
+                return;
+            }
+
+            // Replace spaces and special characters to create a valid image filename
+            var imageName = imgTitle.replace(/[^a-zA-Z0-9 ]/g, "_");
+
+            // Set the image source based on the generated filenames for JPG and PNG formats in the same folder
+            var imagePathJPG = "https://sinbad.hi10anime.com/noro/covers/" + imageName + ".jpg";
+            var imagePathPNG = "https://sinbad.hi10anime.com/noro/covers/" + imageName + ".png";
+
+            // Try loading the image with the JPG extension first, then fallback to PNG if not found
+            loadImage(imagePathJPG, function () {
+                $("#tabImage").attr("src", imagePathJPG);
+            }, function () {
+                $("#tabImage").attr("src", imagePathPNG);
+            });
+        }
+
+        // Call the updateImage function initially
+        updateImage();
+
+        // Helper function to check if an image exists and execute callbacks accordingly
+        function loadImage(src, successCallback, errorCallback) {
+            var img = new Image();
+            img.onload = successCallback;
+            img.onerror = errorCallback;
+            img.src = src;
+        }
+    });

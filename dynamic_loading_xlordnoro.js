ec98db9823382c047fff755d699ab277cff04659
xlordnoro <69708902+xlordnoro@users.noreply.github.com>
2023-12-12 22:56:51 -0500

    Update dynamic_loading_xlordnoro.js
    Fingers crossed!

--- dynamic_loading_xlordnoro.js
+++ dynamic_loading_xlordnoro.js
@@ -1,56 +1,59 @@
-jQuery(function($) {
-        // Function to update the image sources based on the extracted post number
-        function updateImages(postNumber) {
-            var coverImagePath = "https://raw.githubusercontent.com/xlordnoro/xlordnoro.github.io/master/cover_images/" + postNumber + "/cover.jpg";
-            var bd1080ImagePath = "https://raw.githubusercontent.com/xlordnoro/xlordnoro.github.io/master/button_images/" + postNumber + "/bd1080.jpg";
-            var bd720ImagePath = "https://raw.githubusercontent.com/xlordnoro/xlordnoro.github.io/master/button_images/" + postNumber + "/bd720.jpg";
-            var movieImagePath = "https://raw.githubusercontent.com/xlordnoro/xlordnoro.github.io/master/button_images/" + postNumber + "/movie.jpg";
+// Use jQuery(function($)) to ensure the document is ready and $ is available
+jQuery(function ($) {
+    // Function to update the image sources based on the extracted post number
+    function updateImages(postNumber) {
+        var coverImagePath = "https://raw.githubusercontent.com/xlordnoro/xlordnoro.github.io/master/cover_images/" + postNumber + "/cover.jpg";
+        var bd1080ImagePath = "https://raw.githubusercontent.com/xlordnoro/xlordnoro.github.io/master/button_images/" + postNumber + "/bd1080.jpg";
+        var bd720ImagePath = "https://raw.githubusercontent.com/xlordnoro/xlordnoro.github.io/master/button_images/" + postNumber + "/bd720.jpg";
+        var movieImagePath = "https://raw.githubusercontent.com/xlordnoro/xlordnoro.github.io/master/button_images/" + postNumber + "/movie.jpg";
 
-            // Update the cover image
-            loadImage(coverImagePath, function () {
-                $("#coverImage").attr("src", coverImagePath);
-            }, function (error) {
-                console.log("Error loading cover image:", error);
-            });
+        // Update the cover image
+        loadImage(coverImagePath, function () {
+            $("#coverImage").attr("src", coverImagePath);
+            console.log("Cover image loaded successfully!");
+        }, function (error) {
+            console.log("Error loading cover image:", error);
+        });
 
-            // Update the button images
-            updateButtonImage("bd1080", "bd1080on", bd1080ImagePath);
-            updateButtonImage("bd720", "bd720on", bd720ImagePath);
-            updateButtonImage("movie", "movieon", movieImagePath);
-        }
+        // Update the button images
+        updateButtonImage("bd1080", "bd1080on", bd1080ImagePath);
+        updateButtonImage("bd720", "bd720on", bd720ImagePath);
+        updateButtonImage("movie", "movieon", movieImagePath);
+    }
 
-        // Function to update a button image
-        function updateButtonImage(buttonId, buttonOnId, imagePath) {
-            loadImage(imagePath, function () {
-                // Set the source for the button image
-                $("#" + buttonId).attr("href", "#");
-                $("#" + buttonOnId).attr("src", imagePath);
-            }, function (error) {
-                // Handle the case when the button image is not found
-                console.log(buttonId + " image not found:", error);
-            });
-        }
+    // Function to update a button image
+    function updateButtonImage(buttonId, buttonOnId, imagePath) {
+        loadImage(imagePath, function () {
+            // Set the source for the button image
+            $("#" + buttonId).attr("href", "#");
+            $("#" + buttonOnId).attr("src", imagePath);
+            console.log(buttonId + " image loaded successfully!");
+        }, function (error) {
+            // Handle the case when the button image is not found
+            console.log(buttonId + " image not found:", error);
+        });
+    }
 
-        // Call the updateImages function with the extracted post number when the page loads
-        var postNumber = extractPostNumber();
-        updateImages(postNumber);
+    // Call the updateImages function with the extracted post number when the page loads
+    var postNumber = extractPostNumber();
+    updateImages(postNumber);
 
-        // Helper function to check if an image exists and execute callbacks accordingly
-        function loadImage(src, successCallback, errorCallback) {
-            var img = new Image();
-            img.onload = successCallback;
-            img.onerror = function () {
-                errorCallback("Failed to load image: " + src);
-            };
-            img.src = src;
-        }
+    // Helper function to check if an image exists and execute callbacks accordingly
+    function loadImage(src, successCallback, errorCallback) {
+        var img = new Image();
+        img.onload = successCallback;
+        img.onerror = function () {
+            errorCallback("Failed to load image: " + src);
+        };
+        img.src = src;
+    }
 
-        // Function to extract the post number from the URL without the ".html" extension
-        function extractPostNumber() {
-            var currentURL = window.location.href;
-            var urlParts = currentURL.split("/");
-            var lastPart = urlParts[urlParts.length - 1];
-            // Remove ".html" extension if present
-            return lastPart.replace(".html", "");
-        }
+    // Function to extract the post number from the URL without the ".html" extension
+    function extractPostNumber() {
+        var currentURL = window.location.href;
+        var urlParts = currentURL.split("/");
+        var lastPart = urlParts[urlParts.length - 1];
+        // Remove ".html" extension if present
+        return lastPart.replace(".html", "");
+    }
 });

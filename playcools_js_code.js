0af683af2bd81d61b0d44d07feeecb590ab63b62
xlordnoro <69708902+xlordnoro@users.noreply.github.com>
2025-08-30 23:25:37 -0400

    Expanded playcool_js_code.js
    - At Playcool's request, I've expanded his js code file to handle a max of ten seasons vs five seasons as it was previously set at. Gotta love when a series takes 6+ seasons to finish xD

--- playcools_js_code.js
+++ playcools_js_code.js
@@ -1,5 +1,70 @@
 // <![CDATA[
     jQuery(function($) {
+    /* === Tenth season (topmost) === */
+    function show_tenth_season_bd720() {
+        $('#tenth_season_bd1080pane').hide();
+        $('#tenth_season_bd1080on').fadeTo('fast', 0.4);
+        $('#tenth_season_bd720on').fadeTo('fast', 1.0);
+        $('#tenth_season_bd720pane').slideDown('slow', function() {});
+    }
+    function show_tenth_season_bd1080() {
+        $('#tenth_season_bd720pane').hide();
+        $('#tenth_season_bd1080on').fadeTo('fast', 1);
+        $('#tenth_season_bd720on').fadeTo('fast', 0.4);
+        $('#tenth_season_bd1080pane').slideDown('slow', function() {});
+    }
+    $('#tenth_season_bd720').click(function(e){e.preventDefault();show_tenth_season_bd720();});
+    $('#tenth_season_bd1080').click(function(e){e.preventDefault();show_tenth_season_bd1080();});
+
+    /* === Ninth season === */
+    function show_ninth_season_bd720() {
+        $('#ninth_season_bd1080pane').hide();
+        $('#ninth_season_bd1080on').fadeTo('fast', 0.4);
+        $('#ninth_season_bd720on').fadeTo('fast', 1.0);
+        $('#ninth_season_bd720pane').slideDown('slow', function() {});
+    }
+    function show_ninth_season_bd1080() {
+        $('#ninth_season_bd720pane').hide();
+        $('#ninth_season_bd1080on').fadeTo('fast', 1);
+        $('#ninth_season_bd720on').fadeTo('fast', 0.4);
+        $('#ninth_season_bd1080pane').slideDown('slow', function() {});
+    }
+    $('#ninth_season_bd720').click(function(e){e.preventDefault();show_ninth_season_bd720();});
+    $('#ninth_season_bd1080').click(function(e){e.preventDefault();show_ninth_season_bd1080();});
+
+    /* === Eighth season === */
+    function show_eighth_season_bd720() {
+        $('#eighth_season_bd1080pane').hide();
+        $('#eighth_season_bd1080on').fadeTo('fast', 0.4);
+        $('#eighth_season_bd720on').fadeTo('fast', 1.0);
+        $('#eighth_season_bd720pane').slideDown('slow', function() {});
+    }
+    function show_eighth_season_bd1080() {
+        $('#eighth_season_bd720pane').hide();
+        $('#eighth_season_bd1080on').fadeTo('fast', 1);
+        $('#eighth_season_bd720on').fadeTo('fast', 0.4);
+        $('#eighth_season_bd1080pane').slideDown('slow', function() {});
+    }
+    $('#eighth_season_bd720').click(function(e){e.preventDefault();show_eighth_season_bd720();});
+    $('#eighth_season_bd1080').click(function(e){e.preventDefault();show_eighth_season_bd1080();});
+
+    /* === Seventh season === */
+    function show_seventh_season_bd720() {
+        $('#seventh_season_bd1080pane').hide();
+        $('#seventh_season_bd1080on').fadeTo('fast', 0.4);
+        $('#seventh_season_bd720on').fadeTo('fast', 1.0);
+        $('#seventh_season_bd720pane').slideDown('slow', function() {});
+    }
+    function show_seventh_season_bd1080() {
+        $('#seventh_season_bd720pane').hide();
+        $('#seventh_season_bd1080on').fadeTo('fast', 1);
+        $('#seventh_season_bd720on').fadeTo('fast', 0.4);
+        $('#seventh_season_bd1080pane').slideDown('slow', function() {});
+    }
+    $('#seventh_season_bd720').click(function(e){e.preventDefault();show_seventh_season_bd720();});
+    $('#seventh_season_bd1080').click(function(e){e.preventDefault();show_seventh_season_bd1080();});
+    
+	    /* === Sixth season === */	
        function show_sixth_season_bd720() {
             $('#sixth_season_bd1080pane').hide();
             $('#sixth_season_bd1080on').fadeTo('fast', 0.4);
@@ -22,6 +87,8 @@
             event.preventDefault();
             show_sixth_season_bd1080();
         });
+		
+		/* === Fifth season === */	
         function show_fifth_season_bd720() {
             $('#fifth_season_bd1080pane').hide();
             $('#fifth_season_bd1080on').fadeTo('fast', 0.4);
@@ -44,6 +111,8 @@
             event.preventDefault();
             show_fifth_season_bd1080();
         });
+		
+		/* === Fourth season === */	
         function show_fourth_season_bd720() {
             $('#fourth_season_bd1080pane').hide();
             $('#fourth_season_bd1080on').fadeTo('fast', 0.4);
@@ -66,6 +135,8 @@
             event.preventDefault();
             show_fourth_season_bd1080();
         });
+		
+		/* === Third season === */	
         function show_third_season_bd720() {
             $('#third_season_bd1080pane').hide();
             $('#third_season_bd1080on').fadeTo('fast', 0.4);
@@ -88,6 +159,8 @@
             event.preventDefault();
             show_third_season_bd1080();
         });
+		
+		/* === Second season === */	
         function show_second_season_bd720() {
             $('#second_season_bd1080pane').hide();
             $('#second_season_bd1080on').fadeTo('fast', 0.4);
@@ -110,6 +183,8 @@
             event.preventDefault();
             show_second_season_bd1080();
         });
+		
+		/* === First season === */	
         function show_first_season_bd720() {
             $('#first_season_bd1080pane').hide();
             $('#first_season_bd1080on').fadeTo('fast', 0.4);
@@ -138,5 +213,9 @@
         show_fourth_season_bd1080();
         show_fifth_season_bd1080();
         show_sixth_season_bd1080();
+		show_seventh_season_bd1080();
+		show_eighth_season_bd1080();
+		show_ninth_season_bd1080();
+		show_tenth_season_bd1080();
     });
 // ]]>
